fe28445c522497dd06b405da6bb2dc106d4b6d6a
bergeg1 <berg_eg@mail.ru>
2021-12-28 23:10:44 +0500

    Grouping tests and setup (lesson4.3_step13)

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,12 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON).click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
--- test_product_page.py
+++ test_product_page.py
@@ -1,21 +1,42 @@
 from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 import pytest
 import time
-'''
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-'''
+
+
+@pytest.mark.login_user
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        # зарегистрировать нового пользователя;
+        email = str(time.time()) + "@fakemail.org"
+        login_page.register_new_user(email, "P_psw-12345")
+        # проверить, что пользователь залогинен
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_name = product_page.product_name_before_adding()
+        product_cost = product_page.product_cost_before_adding()
+        product_page.add_to_basket()
+        product_page.solve_quiz_and_get_code()
+        product_page.product_name_check_after_adding(product_name)
+        product_page.basket_total_check_after_adding(product_cost)
 
 
 @pytest.mark.xfail
@@ -25,13 +46,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page.open()
     product_page.add_to_basket()
     product_page.solve_quiz_and_get_code()
-    product_page.should_not_be_success_message()
-
-
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    product_page = ProductPage(browser, link)
-    product_page.open()
     product_page.should_not_be_success_message()
 
 
@@ -67,23 +81,3 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_items_message()
     basket_page.should_be_empty_basket_message()
-
-
-'''
-
-def test_guest_can_add_product_to_basket(browser):  #, link
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_product_page()
-    product_name = product_page.product_name_before_adding()
-    product_cost = product_page.product_cost_before_adding()
-    product_page.should_not_be_success_message()
-    product_page.add_to_basket()
-    product_page.solve_quiz_and_get_code()
-    time.sleep(10)
-    product_page.product_name_check_after_adding(product_name)
-    product_page.basket_total_check_after_adding(product_cost)
-
-'''
